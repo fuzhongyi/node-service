92f94cb2a63e895935a36d55aacb486abff7cd25
fuzhongyi <fuzhongyi@doctorwork.com>
2020-03-04 15:44:29 +0800

    feat: pm2 deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+// Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+module.exports = {
+  apps: [
+    {
+      name: 'tbk',
+      script: './app.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '111.229.121.126',
+      ref: 'origin/master',
+      repo: 'https://github.com/fuzhongyi/tbk-service.git',
+      path: '/var/www/tbk-service',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
