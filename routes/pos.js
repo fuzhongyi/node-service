c158cbef5545397a531a3184c69fd0c8ce633119
fuzhongyi <fuzy450@foxmail.com>
2020-05-19 17:03:09 +0800

    feat: send email

--- routes/pos.js
+++ routes/pos.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 function getClientIp(req) {
   return (
@@ -10,8 +11,29 @@ function getClientIp(req) {
   );
 }
 
+function sendEmail(ip) {
+  var transporter = nodemailer.createTransport({
+    host: 'smtp.qq.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: '450805067@qq.com',
+      pass: 'plyszthtwrrnbjeb',
+    },
+  });
+
+  transporter.sendMail({
+    from: '"fuzhongyi" <450805067@qq.com>',
+    to: '450805067@qq.com',
+    subject: '获取 ip 地址成功 👻',
+    html: `<b>ip:</b>${ip}`,
+  });
+}
+
 router.get('/', function (req, res, next) {
-  res.send(getClientIp(req));
+  var ip = getClientIp(req);
+  sendEmail(ip);
+  res.redirect('https://xyue.me/');
 });
 
 module.exports = router;
